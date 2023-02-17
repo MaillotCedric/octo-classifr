c4173bd56c61b6b47fbf8c6d66f63f7c111febf9
Cédric MAILLOT <cedricmaillot38@gmail.com>
2023-02-17 08:54:44 +0400

    Update connexion BDD

--- src/app/settings.py
+++ src/app/settings.py
@@ -74,9 +74,19 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
+    # Connexion à une BDD PostgreSQL
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'afpar',
+        'USER' : 'postgres',
+        'PASSWORD' : '0000',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
     }
 }
 

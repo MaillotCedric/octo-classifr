6f1d3c72427cd0624cfbd4b6ab692af658fb874d
Cédric MAILLOT <cedricmaillot38@gmail.com>
2023-02-23 14:15:31 +0400

    Create model Image

--- src/app_images/admin.py
+++ src/app_images/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Categorie
+from .models import Categorie, Image
 
 admin.site.register(Categorie)
+admin.site.register(Image)
--- src/app_images/models.py
+++ src/app_images/models.py
@@ -7,3 +7,12 @@ class Categorie(models.Model):
     class Meta:
         managed = True
         db_table = 'categorie'
+
+class Image(models.Model):
+    url = models.CharField(primary_key=True, max_length=500)
+    nom_image = models.CharField(max_length=50, blank=True, null=True)
+    id_categorie = models.ForeignKey(Categorie, models.DO_NOTHING, db_column='id_categorie', blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'image'

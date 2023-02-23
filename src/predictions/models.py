2ab68288f031d0d9908300c9d9b22a074971d5ce
Cédric MAILLOT <cedricmaillot38@gmail.com>
2023-02-23 15:35:28 +0400

    Create model Prediction

--- src/predictions/models.py
+++ src/predictions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from modeles.models import Modele
+from app_images.models import Image
 
-# Create your models here.
+class Prediction(models.Model):
+    id_prediction = models.CharField(primary_key=True, max_length=50)
+    etat = models.BooleanField(blank=True, null=True)
+    commentaire = models.CharField(max_length=200, blank=True, null=True)
+    id_modele = models.ForeignKey(Modele, models.DO_NOTHING, db_column='id_modele')
+    url = models.ForeignKey(Image, models.DO_NOTHING, db_column='url')
+
+    class Meta:
+        managed = True
+        db_table = 'prediction'

395d0372d0138c3b35d5786d79848cd7839ffbc2
Cédric MAILLOT <cedricmaillot38@gmail.com>
2023-02-23 13:23:40 +0400

    Create model Modele

--- src/modeles/models.py
+++ src/modeles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Modele(models.Model):
+    id_modele = models.CharField(primary_key=True, max_length=50)
+    nom_modele = models.CharField(max_length=50, blank=True, null=True)
+    accuracy = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+    recall = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+    statut = models.BooleanField(blank=True, null=True)
+    date_fin_entrainement = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'modele'
